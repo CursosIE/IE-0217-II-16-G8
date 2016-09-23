0f82fa7b092145a347492e00cec64711b118754a
DanielD <danidiaza@gmail.com>
2016-09-23 15:17:50 -0600

    Documentación para Doxygen

--- Lab3/include/fraccion.h
+++ Lab3/include/fraccion.h
@@ -1,22 +1,74 @@
+/************************************************
+ * Daniel Díaz Molina                           *
+ * B22245                                       *
+ * Estructuras Abstractas de Datos y Algoritmos *
+ * II - 2016                                    *
+ ************************************************/
+
 #ifndef FRACCION_H
 #define FRACCION_H
 
+/**
+ * Clase que representa una fracción
+ */
 class Fraccion {
 	public:
+		/**
+		 * Constructor por defecto.
+		 * Crea una fracción 1/1.
+		 */
 		Fraccion();
+
+		/**
+		 * Constructor que recibe el numerador y denominador de la fracción.
+		 * @param num Numerador
+		 * @param den Denominador
+		 */
 		Fraccion(int num, int den);
+
+		/**
+		 * Desctructor por defecto
+		 */
 		virtual ~Fraccion();
 
+		/**
+		 * Operador de suma.
+		 * @param other El otro sumando.
+		 * @return Fracción con el resultado.
+		 */
 		Fraccion operator+(const Fraccion &other);
+
+		/**
+		 * Operador de resta.
+		 * @param other El sustrayendo.
+		 * @return Fracción con el resultado.
+		 */
 		Fraccion operator-(const Fraccion &other);
+
+		/**
+		 * Operador de multiplicaión.
+		 * @param other El otro multiplicando.
+		 * @return Fracción con el resultado.
+		 */
 		Fraccion operator*(const Fraccion &other);
+
+		/**
+		 * Operador de división.
+		 * @param other El divisor.
+		 * @return Fracción con el resultado.
+		 */
 		Fraccion operator/(const Fraccion &other);
+
+		/**
+		 * Operador que imprime en pantalla la 
+		 * fracción.
+		 */
 		void operator~ ();
 
 
 	private:
-		int numerador;
-		int denominador;
+		int numerador; /**< Numerador de la fracción. */
+		int denominador; /**< Denominador de la fracción. */
 
 };
 
--- Lab3/include/matriz.h
+++ Lab3/include/matriz.h
@@ -1,26 +1,88 @@
+/************************************************
+ * Daniel Díaz Molina                           *
+ * B22245                                       *
+ * Estructuras Abstractas de Datos y Algoritmos *
+ * II - 2016                                    *
+ ************************************************/
+
 #ifndef MATRIZ_H
 #define MATRIZ_H
 
+/**
+ * Clase que representa una matriz
+ */
 class Matriz {
 	public:
+		/**
+		 * Constructor por defecto
+		 */
 		Matriz();
+
+		/* Constructor que recibe el tamaño de la matriz. Crea una matriz n por m.
+		 * @param n Filas.
+		 * @param m Columnas.
+		 */
 		Matriz(int n, int m);
+
+		/**
+		 * Constructor por copia
+		 */
 		Matriz(const Matriz& other);
+
+		/**
+		 * Destructor por defecto
+		 */
 		virtual ~Matriz();
 
+		/**
+		 * Operador de suma.
+		 * @param other El otro sumando.
+		 * @return Matriz con el resultado.
+		 */
 		Matriz operator+(const Matriz &other);
+
+		/**
+		 * Operador de resta.
+		 * @param other El sustrayendo.
+		 * @return Matriz con el resultado.
+		 */
 		Matriz operator-(const Matriz &other);
+	
+		/**
+		 * Operador de multiplicaión.
+		 * @param other El otro multiplicando.
+		 * @return Matriz con el resultado.
+		 */
 		Matriz operator*(const Matriz &other);
+
+		/**
+		 * Operador de división.
+		 * @param other El divisor.
+		 * @return Matriz con el resultado.
+		 */
 		Matriz operator/(const Matriz &other);
+
+		/**
+		 * Operador que imprime en pantalla la 
+		 * matriz.
+		 */
 		void operator~();
+		
+		/**
+		 * Operador de asignación
+		 */
 		Matriz& operator=(const Matriz &other);
 
+		/**
+		 * Define el contenido de la posición 
+		 * (x,y) de la matriz.
+		 */
 		void setAt(int x, int y, double val);
 
 	private:
-		int n_size;
-		int m_size;
-		double *datos;
+		int n_size; /**< Filas */
+		int m_size; /**< Columnas */
+		double *datos; /**< Arreglo con los datos de la matriz */
 
 };
 
