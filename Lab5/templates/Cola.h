de5d4ca161cee224c5c2b35ad642a30428d33e30
DanielD <danidiaza@gmail.com>
2016-10-26 23:08:39 -0600

    Plantilla de Colas

---
+++ Lab5/templates/Cola.h
@@ -0,0 +1,22 @@
+#ifndef COLA_H
+#define COLA_H
+
+template <typename type_t>
+class Cola {
+	public:
+		Cola();
+		Cola(const Cola& orig);
+		virtual ~Cola();
+
+		virtual const type_t head() = 0;
+		virtual const type_t tail() = 0;
+
+		virtual void pushTail(type_t element) = 0;
+		virtual type_t popHead() = 0;
+
+		virtual const int size() = 0; 
+		virtual const bool empty() = 0;
+		virtual const void imprimir() = 0;
+};
+
+#endif
