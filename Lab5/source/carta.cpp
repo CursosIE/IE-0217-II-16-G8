df9ba72bde66ca1a59067d5220457c8e27f42150
Luis Mora <lmoramora94@gmail.com>
2016-10-28 11:40:01 -0600

    Add class carta

--- Lab5/source/carta.cpp
+++ Lab5/source/carta.cpp
@@ -1,9 +1,27 @@
 #include "carta.h"
+#include <iostream>
+
 
 carta::carta()
 {
     //ctor
 }
+
+carta::carta(char symb)
+{
+
+   if(int(symb) > 1 && int(symb) < 11 ){
+      value = int(symb);
+      }
+    else if(symb == 'J' || symb == 'Q' || symb == 'K'){
+            value = 10;
+          }
+      else if(symb == 'A'){
+              value = 11;
+            }
+        else cerr << "Not valid card value" << endl;
+
+  }
 
 carta::~carta()
 {
