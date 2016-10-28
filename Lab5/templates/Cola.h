e4b023486fb0bf2af3498380b0aa2d9fd3340f49
DanielD <danidiaza@gmail.com>
2016-10-28 13:54:52 -0600

    Definicion de constructor y destructor

--- Lab5/templates/Cola.h
+++ Lab5/templates/Cola.h
@@ -4,14 +4,14 @@
 template <typename type_t>
 class Cola {
 	public:
-		Cola();
-		Cola(const Cola& orig);
-		virtual ~Cola();
+		Cola(){};
+		Cola(const Cola& orig){};
+		virtual ~Cola(){};
 
 		virtual const type_t head() = 0;
 		virtual const type_t tail() = 0;
 
-		virtual void pushTail(type_t element) = 0;
+		virtual void pushTail(const type_t &element) = 0;
 		virtual type_t popHead() = 0;
 
 		virtual const int size() = 0; 
