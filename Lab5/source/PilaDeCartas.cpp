b4d749bf503456d336fd1bb2397b67da93f6c6d5
DanielD <danidiaza@gmail.com>
2016-10-31 03:04:42 -0600

    Metodo barajar

--- Lab5/source/PilaDeCartas.cpp
+++ Lab5/source/PilaDeCartas.cpp
@@ -1,4 +1,5 @@
 #include "PilaDeCartas.h"
+#include <random>
 
 PilaDeCartas::PilaDeCartas()
 {
@@ -10,4 +11,22 @@ PilaDeCartas::~PilaDeCartas()
     //dtor
 }
 
-void Barajar(){}
+void Barajar(){
+	//Primero se hace un arreglo de todos los nodos;
+	node_t* nodos[m_size];
+	node_t* it = m_top;
+	for (int i = 0; i < m_size; i++) {
+		nodos[i] = it;
+		it = it->next;
+	}
+
+	std::uniform_int_distribution<int> rng(0, m_size-1);
+	//Algoritmo Fisher-Yates
+	for (int i = m_size -1; i > 0; i--) {
+		rng.max(i);
+		int j = rng.();
+	}
+		
+
+
+}
