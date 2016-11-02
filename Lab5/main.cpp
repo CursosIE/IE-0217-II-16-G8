56ec77735cbf873659abd9b926837547830f51c1
DanielD <danidiaza@gmail.com>
2016-11-02 17:53:04 -0600

    Estructura del programa principal con mensajes informativos

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -18,14 +18,35 @@ int main(int argc, char* argv[])
 	}
 
 
-	PriorityQueue<char> q1;
+	//PriorityQueue<char> q1;
 	Casino *c = new Casino();
 
-	for (int i = 0; i < strlen(argv[1]); i++) { 
+	for (size_t i = 0; i < strlen(argv[1]); i++) { 
 		c->encolar(argv[1][i]);
 	}
+	char separador[] = "*------------------------------*";
 
-	c->imprimir();
+	cout << endl << "\t\tBienvenido al CASINO!!!!" << endl << endl;
+
+	int i = 1;
+	
+	while (c->hayGente() && i < 10) {
+
+		cout << separador << endl;
+		cout << "\t\tIteración " << i << endl << endl;
+		c->moverGente();
+		cout << "\tInicio:" << endl << endl;
+		c->imprimir();
+		c->correr();
+		cout << endl << "\tFin:" << endl << endl;
+		c->imprimir();
+		i++;
+
+	}
+
+	cout << separador << endl;
+	cout << "\t\tFIN del Programa!" << endl;
+	return i;
 
 	// Ejemplo de como usar la cola
 //	cout << "Creando mesilla!" << endl;
