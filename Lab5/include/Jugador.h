81e2c9d59262d0d31be5bc361b22a76509035220
DanielD <danidiaza@gmail.com>
2016-11-02 20:16:41 -0600

    Documentación Doxygen

--- Lab5/include/Jugador.h
+++ Lab5/include/Jugador.h
@@ -5,23 +5,58 @@
 #include "carta.h"
 #include <list>
 
+/**
+ * Clase que representa a un jugador
+ * sentada en alguna mesa del casino.
+ */
 class Jugador {
 	public:
+		/**
+		 * Constructor por defecto.
+		 */
 		Jugador();
+
+		/**
+		 * Constructor que recibe el tipo de persona del jugador.
+		 * @param tipo Caracter que representa el tipo de persona.
+		 */
 		Jugador(char tipo);
+
+		/**
+		 * Destructor por defecto.
+		 */
 		virtual ~Jugador();
 
 
+		/**
+		 * Reparte una carta al jugador, lo cual hace que aumente
+		 * su puntaje por el valor correspondiente.
+		 * @param c La carta que recibe el jugador.
+		 */
 		void repartir(carta c);
+
+		/**
+		 * Reinicia el puntaje del jugador (lo pone en cero).
+		 */
 		void reset();
+
+		/**
+		 * Indica los puntos de la mano del jugador.
+		 * @return Cantidad de puntos que tiene.
+		 */
 		int puntos() const;
+
+		/**
+		 * Indica el tipo de persona que es el jugador.
+		 * @return Tipo (E,T,D).
+		 */
 		char tipo() const;
 
 
 		
 	private:
-		char m_tipo;
-		int m_score;
+		char m_tipo; /**<Caracter que representa el tipo de persona.*/
+		int m_score; /**<Puntaje de la mano del jugador.*/
 
 };
 
--- Lab5/templates/PriorityQueue.h
+++ Lab5/templates/PriorityQueue.h
@@ -5,16 +5,36 @@
 #include "LinkedNode.h"
 #include <iostream> 
 
+/**
+ * Plantilla de una Cola con prioridad.
+ * La cola utiliza nodos doblemente enlazados.
+ * @tparam type_t El tipo de datos que guarda la cola.
+ */
 template <typename type_t>
 class PriorityQueue : public Cola<type_t> {
 	public:
 		typedef LinkedNode<type_t> node_t;
+
+		/**
+		 * Constructor por defecto.
+		 */
 		PriorityQueue(): m_head(0), m_tail(0), m_size(0){};
+
 		PriorityQueue(const PriorityQueue& orig){};
+
+		/**
+		 * Destructor por defecto.
+		 */
 		virtual ~PriorityQueue(){
 			clear();
 		};
 
+		/**
+		 * Elemento a la cabeza de la cola.
+		 * Si la cola está vacía el comportamiento
+		 * es indefinido.
+		 * @return El elemento.
+		 */
 		type_t head() const{
 			if (m_head) {
 				return m_head->data;
@@ -24,6 +44,12 @@ class PriorityQueue : public Cola<type_t> {
 			}
 		};
 
+		/**
+		 * Elemento al final de la cola.
+		 * Si la cola está vacía el comportamiento
+		 * es indefinido.
+		 * @return El elemento
+		 */
 		type_t tail() const{
 			if (m_tail) {
 				return m_tail->data;
@@ -33,6 +59,12 @@ class PriorityQueue : public Cola<type_t> {
 			}
 		};
 
+		/**
+		 * Agrega un elemento al final de la cola.
+		 * Crea un nuevo nodo que contenga al elemento
+		 * y lo enlaza al final de la cola.
+		 * @param element El elemento que se agrega.
+		 */
 		void pushTail(const type_t &element){
 			if (m_size == 0) {
 				m_head = new node_t(0, 0, element);
@@ -48,6 +80,13 @@ class PriorityQueue : public Cola<type_t> {
 			return;
 		};
 
+		/**
+		 * Saca el elemento que está de primero en la cola.
+		 * Retorna este elemento y luego lo elimina de la
+		 * cola. Si la cola está vacía su comportamiento
+		 * es indefinido.
+		 * @return El primer elemento de la cola.
+		 */
 		type_t popHead(){
 			if (m_size == 0) {
 				std::cerr << "Trying to pop m_tail of empty queue." << std::endl;
@@ -71,15 +110,32 @@ class PriorityQueue : public Cola<type_t> {
 			return data;
 		};
 
+		/**
+		 * Retorna el tamaño de la cola.
+		 * @return La cantidad de elementos de la cola.
+		 */
 		int size() const{
 			return m_size;
 		};
 
 
+		/**
+		 * Indica si la cola se encuentra vacía.
+		 * @return True si está vacía.
+		 */
 		bool empty() const{
 			return m_size == 0;
 		};
 
+		/**
+		 * Imprime información de la cola.
+		 * Indica la cantidad de elementos y luego 
+		 * imprime los elementos separados por espacios.
+		 * El tipo de la plantilla (type_t) debe definir
+		 * el operador '<<' para poder concatenar al 
+		 * stream std::cout, de lo contrario habrá un error
+		 * de compilación.
+		 */
 		void imprimir() const{
 			std::cout << "Cola de prioridad con " << m_size << " elementos." << std::endl;
 			node_t *it = m_head;
@@ -92,10 +148,13 @@ class PriorityQueue : public Cola<type_t> {
 
 	protected:
 
-		node_t* m_head;
-		node_t* m_tail;
-		int m_size;
+		node_t* m_head; /**<Puntero al nodo que contiene el primer elemento de la cola.*/
+		node_t* m_tail; /**<Puntero al nodo que tiene el último elemento de la cola.*/
+		int m_size; /**<Cantidad de elementos en la cola.*/
 
+		/**
+		 * Borra todos los elementos de la cola y deja su tamaño en cero.
+		 */
 		void clear(){
 			node_t* it;
 			it = m_head;
